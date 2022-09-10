df4a95cf63e64eb31ca97ff825c55238a2ae9f59
LewBlu <lewis.blundell.development@gmail.com>
2022-09-10 23:19:02 +0100

    Angular CDK added to app, used for DragDrop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +27,8 @@ import { MyProjectsComponent } from './dashboard/my-projects/my-projects.compone
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
