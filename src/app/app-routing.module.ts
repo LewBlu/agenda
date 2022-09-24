0981cab523118f93ccaabf4cfff93055cfd8a5ce
LewBlu <lewis.blundell.development@gmail.com>
2022-09-24 23:46:43 +0100

    Guard added to routes, requires connect.sid cookie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
-	{ path: '', component: DashboardComponent },
-	{ path: 'projects', component: ProjectsComponent },
+	{ path: '', component: DashboardComponent, canActivate: [AuthGuard] },
+	{ path: 'projects', component: ProjectsComponent, canActivate: [AuthGuard] },
 	{ path: 'login', component: LoginComponent },
 ];
 
